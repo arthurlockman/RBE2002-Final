b57e7c23e0bc45311e109a0c0b368f93bf274810
Arthur Lockman <hello@rthr.me>
2014-11-02 22:57:30 -0500

    Added preliminary mouse code.

--- Pi_Runtime/Main.py
+++ Pi_Runtime/Main.py
@@ -1,7 +1,32 @@
 import atexit
 from time import sleep
 from Arduino import *
+from multiprocessing import Process, Pipe
 
 ARDUINO = Arduino()
+MOUSE = file('/dev/input/mouse0')
+
+
+def read_mouse(mouse, conn):
+    while True:
+        status, dx, dy = tuple(ord(c) for c in mouse.read(3))
+
+        def to_signed(n):
+            return n - ((0x80 & n) << 1)
+
+        dx = to_signed(dx)
+        dy = to_signed(dy)
+        conn.send([status, dx, dy])
+
+
+def kill_daemons():
+    mouse_d.terminate()
 
 atexit.register(close_serial_port)
+atexit.register(kill_daemons)
+
+if __name__ == '__main__':
+    main_conn, mouse_conn = Pipe()
+    mouse_d = Process(target=read_mouse, args=(MOUSE, mouse_conn, ))
+    mouse_d.start()
+    print main_conn.get()
