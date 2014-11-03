e41c17e70300642c8fae91eeea8ee82bcf3723f6
Arthur Lockman <hello@rthr.me>
2014-11-02 22:07:42 -0500

    Cleaned formatting of python runtime.

--- Pi_Runtime/Main.py
+++ Pi_Runtime/Main.py
@@ -1,12 +1,14 @@
-import serial
+from serial import Serial
 import atexit
 from time import sleep
 
-ARDUINO_COMM = serial.Serial("/dev/ttyACM0", 115200)
+ARDUINO_COMM = Serial("/dev/ttyACM0", 115200)
 
-def closeSerialPort():
-	ARDUINO_COMM.close()
-	print "Closing serial port..."
 
-atexit.register(closeSerialPort)
+def close_serial_port():
+    """Closes a serial port at program exit."""
+    ARDUINO_COMM.close()
+    print "Closing serial port..."
 
+
+atexit.register(close_serial_port)
