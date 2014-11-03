03a17980242386a0769e3fa53d220f909cb4b488
Arthur Lockman <hello@rthr.me>
2014-11-02 23:00:08 -0500

    Won't crash if serial port isn't open.

--- Pi_Runtime/Arduino.py
+++ Pi_Runtime/Arduino.py
@@ -1,17 +1,20 @@
 from serial import Serial
 import sys
 
-if sys.platform.startswith('darwin'):
-    ARDUINO_COMM = Serial('/dev/tty.usbmodem1411', 115200)
-elif sys.platform.startswith('linux2'):
-    ARDUINO_COMM = Serial("/dev/ttyACM0", 115200)
-elif sys.platform.startswith('win32'):
-    ARDUINO_COMM = Serial("COM1", 115200)
-
+try:
+    if sys.platform.startswith('darwin'):
+        ARDUINO_COMM = Serial('/dev/tty.usbmodem1411', 115200)
+    elif sys.platform.startswith('linux2'):
+        ARDUINO_COMM = Serial("/dev/ttyACM0", 115200)
+    elif sys.platform.startswith('win32'):
+        ARDUINO_COMM = Serial("COM1", 115200)
+except OSError:
+    "No serial port."
 
 def close_serial_port():
     """Closes a serial port at program exit."""
-    ARDUINO_COMM.close()
+    if ARDUINO_COMM.isOpen():
+        ARDUINO_COMM.close()
     print "Closing serial port..."
 
 
@@ -22,5 +25,6 @@ class Arduino:
 
     def send(self, message):
         """Send a message over the serial channel. Will be automatically terminated with a newline."""
-        ARDUINO_COMM.flush()
-        ARDUINO_COMM.write(message + '\\n')
+        if ARDUINO_COMM.isOpen():
+            ARDUINO_COMM.flush()
+            ARDUINO_COMM.write(message + '\\n')
