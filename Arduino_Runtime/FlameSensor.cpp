9818dc922d7926465698b09b4ced673d5bb60f34
Arthur Lockman <hello@rthr.me>
2014-12-11 18:47:51 -0500

    Added flame sensor distance and height functions.

--- Arduino_Runtime/FlameSensor.cpp
+++ Arduino_Runtime/FlameSensor.cpp
@@ -6,7 +6,35 @@ FlameSensor::FlameSensor(int port):
     //Init complete.
 }
 
-int FlameSensor::read()
+int FlameSensor::rawRead()
 {
     return analogRead(m_port);
+}
+
+int FlameSensor::read()
+{
+    int sum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        sum += this->rawRead();
+    }
+    return sum / 10;
+}
+
+float FlameSensor::distance()
+{
+	int y = this->read();
+	if (y < K_CUTOFF)
+	{
+		double base = K_C * ((y - K_A) / (K_D - y));
+		double power = 1 / K_B;
+		return pow(base, power);
+	}
+	else { return -1.0; }
+}
+
+float FlameSensor::height()
+{
+	int x = this->read();
+	return 0.0;
 }
--- Arduino_Runtime/FlameSensor.h
+++ Arduino_Runtime/FlameSensor.h
@@ -3,11 +3,20 @@
 
 #include "Arduino.h"
 
+#define K_A 23.92856
+#define K_B 7.783453
+#define K_C 25.06772
+#define K_D 849.9155
+#define K_CUTOFF 1000
+
 class FlameSensor
 {
 public:
     FlameSensor(int port);
     int read();
+    int rawRead();
+    float distance();
+    float height();
 private:
     int m_port;    
 };
